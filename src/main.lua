dda0a6cdd990ca595ca3e74389c56aa633e9ece0
Shimon Vaisberg <shimon.vaisberg@overwolf.com>
2024-05-29 17:25:14 +0300

    first version - 0.0.1

--- src/main.lua
+++ src/main.lua
@@ -18,20 +18,16 @@ rom = rom
 _PLUGIN = _PLUGIN
 
 -- get definitions for the game's globals
----@module 'game'
+---@module 'SGG_Modding-Hades2GameDef-Globals'
 game = rom.game
 ---@module 'game-import'
 import_as_fallback(game)
 
----@module 'SGG_Modding-SJSON'
-sjson = mods['SGG_Modding-SJSON']
 ---@module 'SGG_Modding-ModUtil'
 modutil = mods['SGG_Modding-ModUtil']
 
 ---@module 'SGG_Modding-Chalk'
 chalk = mods["SGG_Modding-Chalk"]
----@module 'SGG_Modding-ReLoad'
-reload = mods['SGG_Modding-ReLoad']
 
 ---@module 'config'
 config = chalk.auto 'config.lua'
@@ -41,19 +37,24 @@ public.config = config -- so other mods can access our config
 local function on_ready()
 	-- what to do when we are ready, but not re-do on reload.
 	if config.enabled == false then return end
-	
-	import 'ready.lua'
+
+	local ShieldChargeStages = game.DeepCopyTable(game.WeaponData.WeaponAxeBlock2.ChargeWeaponStages)
+	local FirstStage = ShieldChargeStages[1]
+	FirstStage.ForceRelease = false
+	ShieldChargeStages[1] = FirstStage
+	ShieldChargeStages[2] = {
+		DeferSwap = FirstStage.DeferSwap,
+		Wait = config.axeGracePeriod,
+		ForceRelease = true,
+	}
+	game.WeaponData.WeaponAxeBlock2.ChargeWeaponStages = game.DeepCopyTable(
+		ShieldChargeStages)
 end
 
 local function on_reload()
 	-- what to do when we are ready, but also again on every reload.
 	-- only do things that are safe to run over and over.
-	
-	import 'reload.lua'
 end
-
--- this allows us to limit certain functions to not be reloaded.
-local loader = reload.auto_single()
 
 -- this runs only when modutil and the game's lua is ready
 modutil.on_ready_final(function()
